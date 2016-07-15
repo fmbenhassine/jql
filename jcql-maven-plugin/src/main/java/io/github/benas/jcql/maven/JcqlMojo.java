ad100dd07d8574f2ebb210a31dd10357dab6d9d2
Mahmoud Ben Hassine <mahmoud.benhassine@icloud.com>
2016-07-15 09:53:59 +0200

    handle URISyntaxException in Mojo

--- jcql-maven-plugin/src/main/java/io/github/benas/jcql/maven/JcqlMojo.java
+++ jcql-maven-plugin/src/main/java/io/github/benas/jcql/maven/JcqlMojo.java
@@ -31,6 +31,7 @@ import org.apache.maven.plugins.annotations.Parameter;
 
 import java.io.File;
 import java.io.IOException;
+import java.net.URISyntaxException;
 
 @Mojo(name = "index", aggregator = true)
 public class JcqlMojo extends AbstractMojo {
@@ -47,7 +48,7 @@ public class JcqlMojo extends AbstractMojo {
         try {
             indexer.index(sourceCodeDirectory, databaseDirectory);
             getLog().info("Done.");
-        } catch (IOException e) {
+        } catch (IOException | URISyntaxException e) {
             throw new MojoExecutionException("Unable to index jcql database in " + sourceCodeDirectory, e);
         }
     }
